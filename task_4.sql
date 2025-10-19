6632eb13ce84b1edfd5638f16f61d2a7c10154fe
Alexander <bizzle.teye@gmail.com>
2025-10-19 13:11:27 -0700

    commit task 4

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Retrieve full column details for the 'Books' table
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
